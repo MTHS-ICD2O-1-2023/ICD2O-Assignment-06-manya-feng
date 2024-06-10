82586ce0acaa7a4ad4ee14380f929b5fa415ae0d
Manya Feng <159185413+manya-feng@users.noreply.github.com>
2024-06-10 02:00:55 +0000

    change

--- js/script.js
+++ js/script.js
@@ -1,5 +1,26 @@
-// Copyright (c) 2020 Mr. Coxall All rights reserved
+// Copyright (c) 2024 Manya All rights reserved
 //
-// Created by: Mr. Coxall
-// Created on: Sep 2020
+// Created by: Manya
+// Created on: June 2024
 // This file contains the JS functions for index.html
+
+"use strict"
+
+  const getRandomCatImage = async () => {
+    try {
+  const response = await fetch("https://api.thecatapi.com/v1/images/search")
+  const data = await response.json()
+  const dogImageUrl = data.message
+  console.log(dogImageUrl)
+
+  // Display the image on the webpage
+  const dogImageElement = document.createElement("img")
+  dogImageElement.src = dogImageUrl
+  dogImageElement.alt = "Cat image"
+  document.body.appendChild(catImageElement)
+  } catch (error) {
+  console.log(error)
+    }
+  }
+
+  getRandomDogImage()
